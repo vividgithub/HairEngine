f43944b552d3d6a2799ed2e8dd21c21a297b2876
Vivid Home PC - 1 <vividmailbox@163.com>
2018-07-22 21:20:13 +0800

    Add project files.

---
+++ HairEngine/HairEngine.cpp
@@ -0,0 +1,12 @@
+﻿// HairEngine.cpp : Defines the entry point for the application.
+//
+
+#include "HairEngine.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
